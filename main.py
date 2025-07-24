4dc82ca1ad68d494bcd7331cc306b509e788359d
Xlevens <rizwanellahi_@hotmail.com>
2025-07-24 16:40:28 +0500

    added chatbots

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 from Modules.user import UserFront
-from Modules.chat import Chat
-CB = Chat("Rizwan")
-print(CB.greet())
-query = input()
+from Modules.Chat import MainChat
+from Modules.Ronin import RoninChat
+from Modules.Zubaidas import ZubaidaChat
+from Modules.Khaadi import KhaadiChat
 
-print(CB.queryset(query))
+CB = MainChat("Rizwan")
+
+print(CB.req())
+res = input()
+msg, b = CB.response(res)
+print(msg)
+
+
+while True:
+        query = input("You: ")
+        
+        if query.lower() in ["exit", "quit", "bye"]:
+            print("Chat ended. Goodbye!")
+            break
+
+        response = b.queryset(query)
+        print(response)
+
+        if "goodbye" in response.lower() or "bye" in response.lower():
+            print("Chat ended. Goodbye!")
+            break
